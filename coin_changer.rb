d919f41f403963c8ebc1aeed07057fb48f2364a8
andyhaynes13 <admin@haynescomputerservice.com>
2017-07-18 09:26:07 -0400

    change

---
+++ coin_changer.rb
@@ -0,0 +1,4 @@
+def make_change(cents_received)
+	{}
+end
+
---
+++ coin_changer_test.rb
@@ -0,0 +1,24 @@
+require "minitest/autorun"
+require_relative "coin_changer.rb"
+
+class TestCoinChanger < Minitest::Test
+	def test_assert_that_1_equals_1
+		assert_equal(1, 1)
+	end
+
+def test_assert_that_0_cents_equals_empty_hash
+	assert_equal({},make_change(0))
+end
+
+
+def test_assert_that_1_equals_penny
+	assert_equal({"penny" => 1},make_change(1))
+end
+
+def test_assert_that_2_cents_equals_two_pennies
+	assert_equal({"penny" => 2},make_change(2))
+end
+
+
+
+end
